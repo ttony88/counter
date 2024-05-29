33c1fc97d962a5a6fb81e7a95b7ed006a84e1831
ttony62 <1988anton62@gmail.com>
2024-05-29 23:33:08 +0300

    update Counter

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,43 +1,35 @@
-import React, {FC, useEffect, useState}  from 'react'
+import React, {Dispatch, FC}  from 'react'
 import style from './Counter.module.css'
 import { Button } from '../components/button/Button'
-import { getFromLocalStoredgeValue } from '../util/localStorege'
 
+type CounterPropsType = {
+    count: string
+    messageCount: string
+    setCount: Dispatch<any>
+    increment: () => void
+    decriment: () => void
+    disabledIncrement: boolean
+    disabledDecrement: boolean
+}
 
-
-export const Counter:FC = () => {
-
-    const maxValue = getFromLocalStoredgeValue("maxValue")
-    const minValue = getFromLocalStoredgeValue("minValue")
-
-    
-
-    const [count, setCount] = useState(minValue)
-
-    const increment = () => {
-       if(count < maxValue){
-           setCount(count + 1)
-       } 
-    }
-    
-    const decriment = () => setCount(0)
-
-    const disabledIncrement = count >= maxValue
-
-    const disabledDecrement = count <= minValue
+export const Counter:FC<CounterPropsType> = (props) => {
 
     const styleDisplay = {
-        color: disabledIncrement ? "red" : "rgb(146, 238, 41)"
+        color: props.disabledIncrement ? 'red' : 'rgb(146, 238, 41)',
+        fontSize: props.messageCount ? 16 : 70,
     }
+
+    const valueCount = props.messageCount ? props.messageCount : props.count
 
     return(
         <div className={style.counter}>
             <div className={style.display} style={styleDisplay}>
-                {count}
+                {valueCount}
             </div>
             <div className={style.buttons}>
-                <Button title='inc' onClickHandler={increment} disabled={disabledIncrement}/>
-                <Button title='reset' onClickHandler={decriment} disabled={disabledDecrement}/>
+                <Button title='inc' onClickHandler={props.increment} disabled={props.disabledIncrement}/>
+                <Button title='reset' onClickHandler={props.decriment} disabled={props.disabledDecrement}/>
+                <Button title='set' onClickHandler={props.decriment} />
             </div>
         </div>
     )
