fa175f77d5ad1bbe504d87ea028f42a9c26ff425
ttony62 <1988anton62@gmail.com>
2024-05-29 23:31:20 +0300

    update Input

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,21 +1,41 @@
-import React, {ChangeEvent, FC}  from 'react'
+import React, {ChangeEvent, Dispatch, FC, KeyboardEvent}  from 'react'
 import style from './Input.module.css'
 
 type InputProps = {
     title: string
     value: number
-    onChangeInput: (value: number) => void
+    onChangeInput: (value: number, setValue: (value: number) => void) => void
+    onFocusInput: () => void
+    onKeyEnter: () => void
+    setValue: Dispatch<any>
 }
 export const Input:FC<InputProps> = (props) => {
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {props.onChangeInput(+e.currentTarget.value)}
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.onChangeInput(+e.currentTarget.value, props.setValue)}
 
+    const onFocusHandler = () => {props.onFocusInput()} 
+
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter') {
+            
+            props.onKeyEnter()
+        }
+    }
+
+    
+    
     return(
         <div className={style.wrapper}>
             <span className={style.title}>
                 {props.title}
             </span>
-            <input type='number' value={props.value} onChange={onChangeHandler}/>
+            <input type='number' 
+                   value={props.value} 
+                   onChange={onChangeHandler}
+                   onFocus={onFocusHandler}
+                   onKeyDown={onKeyDownHandler}
+                   className={style.input}/>
         </div>
     )
 }
